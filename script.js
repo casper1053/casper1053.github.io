ea7ac9e55fa702e71b377a9759d3b5950caa2145
casper1053 <ernycheese@gmail.com>
2024-12-03 13:03:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 var login_screen = document.getElementById("login-screen");
-var content_Screen = document.getElementById("content");
+var content_screen = document.getElementById("content");
+var password_input = document.getElementById("submit button");
+var submit_button = document.getElementById("input");
+function set_css(element, property, value){
+  element.style[property]=value
+}
+function hide_login_screen(){
+  set_css(login_screen, "display", "none");
+}
+function show_login_screen(){
+  set_css(login_screen, "display", "default");
+}
+function show_content_screen(){
+  set_css(content_screen, "display", "default");
+}
+function hide_content_screen(){
+  set_css(content_screen, "display", "none");
+}
