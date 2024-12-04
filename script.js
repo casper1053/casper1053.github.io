5b05d7e17b764113a44e33d1cadc7f5beb48a02b
casper1053 <ernycheese@gmail.com>
2024-12-04 14:27:37 -0800

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var css_ver = 0;
+var css_version = 0;
 var login_screen = document.getElementById("login-screen");
 var content_screen = document.getElementById("content");
-var password_input = document.getElementById("submit button");
-var submit_button = document.getElementById("input");
+var password_input = document.getElementById("input");
+var submit_button = document.getElementById("submit button");
+var passkey_button = document.getElementById("passkey-button");
+var passkey_input = document.getElementById("passkey");
+var css_ver_display = document.getElementById("css-ver-display");
 function set_css(element, property, value){
   element.style[property]=value
 }
@@ -18,3 +21,4 @@ function show_content_screen(){
 function hide_content_screen(){
   set_css(content_screen, "display", "none");
 }
+css_ver_display.innerHTML=`Version ${css_Version}`
